f00ae8e2dddddf63eb1a2adb61b4bafdf4eaf340
lindavtran <56659503+lindavtran@users.noreply.github.com>
2020-10-30 00:53:32 -0400

    fixed send public key

--- server.py
+++ server.py
@@ -6,10 +6,9 @@ import Crypto
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto import Random
-import base64
 
 HOST = '127.0.0.1'  # Localhost
-PORT = 3030       # Listening port
+PORT = 3030         # Listening port
 
 def startServer():
     privatekey, publickey = rsakeys()
@@ -19,20 +18,19 @@ def startServer():
         s.listen()
         conn, addr = s.accept()     # Blocking call
         with conn:
-            while True: #Wait for client_hello
-                data = recvClientHello(conn) #Received Client_Hello
+            while True:                         # 1. Wait for client_hello
+
+                data = conn.recv(1024)          # check for client's message
                 if not data:
                     break
-                serverHello(conn) # Send Server_Hello
 
-                cipher_text = encrypt(publickey, b'here is software')
-                conn.sendall(cipher_text)
+                recvFromClient(data)            # 3. Received Client_Hello
+                serverHello(conn)               # 5. Send Server_Hello
+                sendPublicKey(conn,publickey)   # 6. Send Public key
+                encrypt(conn, publickey, b'here is software') # 8. Send encypted message to client
 
-
-def recvClientHello(s):
-    clientHello = s.recv(1024) # Wait for client_hello message
-    print('Server Received, ', repr(clientHello))
-    return clientHello
+def recvFromClient(data):
+    print('Server Received, ', repr(data))
 
 def serverHello(s):
     # Send serverHello message with right contents
@@ -45,13 +43,16 @@ def rsakeys():
      publickey = privatekey.publickey()
      return privatekey, publickey
 
-#encryption for server
-def encrypt(publickey, plain_text):
+def sendPublicKey(s, publickey):
+    s.send(publickey.exportKey(format='PEM', passphrase=None, pkcs=1))
+
+# Encryption with RSA Public Key from Server to Client
+def encrypt(s, publickey, plain_text):
     cipher = PKCS1_OAEP.new(publickey)
     cipher_text = cipher.encrypt(plain_text)
-    print('Plain text: ', plain_text )
-    print('Cipher text: ', cipher_text)
-    return cipher_text
+    print('     Plain text to encrypt: ', plain_text )
+    print('     Cipher text to send to client: ', cipher_text)
+    s.sendall(cipher_text)
 
 
 print('Server listening on: ', PORT)
