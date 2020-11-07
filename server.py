ab8bfcdbf7355d91d43a797a60ed90a8270e320b
Xiaocheng Chen <rachelchen07216@gmail.com>
2020-11-07 00:22:21 -0500

    generate certificate

--- server.py
+++ server.py
@@ -3,17 +3,18 @@ import json
 from cryptography.hazmat.primitives.hashes import SHA1
 from cryptography.hazmat.primitives.ciphers.algorithms import TripleDES
 
-# import Crypto
-# from Crypto.Cipher import PKCS1_OAEP
-# from Crypto.PublicKey import RSA
-# from Crypto import Random
+import Cryptodome
+from Cryptodome.Cipher import PKCS1_OAEP
+from Cryptodome.PublicKey import RSA
+from Cryptodome import Random
+
 
 HOST = '127.0.0.1'  # Localhost
 PORT = 3030         # Listening port
 
 def startServer():
-    # privatekey, publickey = rsakeys()
-    # print(privatekey, publickey) #Private and public keys
+    privatekey, publickey = rsakeys()
+    print(privatekey, publickey) #Private and public keys
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.listen()
@@ -27,30 +28,43 @@ def startServer():
                     print("Got error receiving Client hello")
                     break
                 sendServerHello(conn)               # 5. Send Server_Hello
-                # sendPublicKey(conn,publickey)       # 6. Send Public key
+                sendCertificate(conn,publickey)     # 6. Send Public key
+                sendServerDone(conn)                # 7. Send Server_hello_done
+                
                 # encrypt(conn, publickey, b'here is software') # 8. Send encypted message to client
 
 def recvClientHello(conn):
     data = conn.recv(1024)
     if not data:
-        return None
+        return None, None, None
     cipherSuit = json.loads(data) # Contains a dictionary with the cyphersuit
-    print(cipherSuit["Cipher"])
-    return 1, cipherSuit, data
+    return 1, cipherSuit, data.decode("utf-8")
 
 def sendServerHello(s):
     # Send serverHello message with right contents
     s.sendall(b'Server_Hello')
+    
+def sendServerDone(s):
+    # Send serverHello message with right contents
+    s.sendall(b'Server_Hello_Done')
 
 #Private key and public key 1024 bits
-# def rsakeys():
-#      length=1024
-#      privatekey = RSA.generate(length, Random.new().read)
-#      publickey = privatekey.publickey()
-#      return privatekey, publickey
+def rsakeys():
+    length=1024
+    privatekey = RSA.generate(length, Random.new().read)
+    publickey = privatekey.publickey()
+    return privatekey, publickey
 
-def sendPublicKey(s, publickey):
-    s.send(publickey.exportKey(format='PEM', passphrase=None, pkcs=1))
+def sendCertificate(s, publickey):
+    certificate_1 = b'{ "Name": "I am server hahahahahahaha", "Serial Number":"12345678", "Expire Date":"12/12/2020", "Public Key of the server":"%s"}' % publickey
+    DS_thirdparty = SHA1(certificate_1)
+    length=192
+    DS_thirdparty_privatekey = RSA.generate(length, Random.new().read)
+    DS_thirdparty_publickey = DS_thirdparty_privatekey.publickey()
+    Enc_DS_thirdparty_privatekey = TripleDES(DS_thirdparty_privatekey)
+    certificate_2 = certificate_1 + b'{"Digital Signature of the third party": "%s", "Public Key of the third party":"%s"}' % (Enc_DS_thirdparty_privatekey, DS_thirdparty_publickey)
+    s.sendall(certificate_2)
+    return certificate_2.decode("utf-8")
 
 # Encryption with RSA Public Key from Server to Client
 def encrypt(s, publickey, plain_text):
