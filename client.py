641cc58bdcd72623c2c850562e66d88ed802b013
Sebastian C <sebascaicedo-01@hotmail.com>
2020-11-15 21:52:06 -0500

    Added the premaster key, both client and server use own implementation of RSA

--- client.py
+++ client.py
@@ -3,27 +3,27 @@ import certificate
 import random
 import json
 import os
+import RSA
 
-import Cryptodome
-from Cryptodome.Cipher import PKCS1_OAEP
-from Cryptodome.PublicKey import RSA
-from Cryptodome import Random
+
 from Cryptodome.Hash import SHA1
 
 # Documentation for cryptography library https://cryptography.io/en/latest/
 HOST = '127.0.0.1'  # Localhost
 PORT = 3030         # Same port as server
+thirdPartyPrivateKey, thirdPartyPublicKey  = RSA.generate_keypair(1297279, 1297657)
 
 def startClient():
     allMsgsHash = SHA1.new()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        allMsgsHash.update(sendClientHello(s))          # 2. Send Client_Hello
-        allMsgsHash.update(recvServerHello(s))          # 4. Receive Server_Hello
-        allMsgsHash.update(recvCertiFromServer(s))      # 6. Receive Certificate from server and generate pre-master key
-        allMsgsHash.update(recvPublicKeyFromServer(s))  # 7. Receive Public Key from server
-        allMsgsHash.update(recvCipherTextFromServer(s)) # 9. Receive CipherText
+        allMsgsHash.update(sendClientHello(s))          # 1. Send Client_Hello
+        allMsgsHash.update(recvServerHello(s))          # 2. Receive Server_Hello
+        serverPublicKey, serverCertificate = recvCertiFromServer(s)
+        allMsgsHash.update(serverCertificate)      # 6. Receive Certificate from server and generate pre-master key
+        preMasterSecret = sendPreMasterSecret(s, serverPublicKey) 
         allMsgsHash.update(recvServerHelloDone(s))      # 11. Receive Server_hello_done
+        return
 
 # 2. Send Client_Hello
 def sendClientHello(s):
@@ -37,14 +37,36 @@ def recvServerHello(s):
     print('Client received (Server Hello), ', repr(serverHello))
     return serverHello
 
-# 6. Receive Certificate from server and generate pre-master key
+# 6. Receive Certificate from server
 def recvCertiFromServer(s):
-    certificate = s.recv(96)
-    print('Client received certificate', certificate)
-    return certificate
+    rawCertificate = s.recv(500)
+    jsonCert = json.loads(rawCertificate)
+    print('Client received certificate', jsonCert)
+    certificate = jsonCert["certificate"]
+    signature = jsonCert["signature"]
+    serverPublicKey = (int(certificate["d"]), int(certificate["n"]))
+    verifySignature(certificate, signature)
+
+    return serverPublicKey, rawCertificate
     #TODO: generate pre-master key(48 byte encrypted with server's RSA public key)
 
-def preMasterKey():
+def sendPreMasterSecret(s, pk):
+    preMasterSecret = generatePreMasterSecret()
+    print("preMasterSecret : ", preMasterSecret)
+    temp = encrypt(pk, preMasterSecret)
+    encryptedPreMasterSecret = ' '.join([str(n) for n in temp])
+    s.sendall(encryptedPreMasterSecret.encode("utf-8"))
+    return preMasterSecret
+
+def verifySignature(contents, signature):
+    certHash = SHA1.new()
+    certHash.update(json.dumps(contents).replace(" ","").encode("utf-8"))
+    intArrOfSignature = [int(n) for n in signature.split()]
+    decryptedHash = decrypt(thirdPartyPublicKey, intArrOfSignature)
+    if decryptedHash != certHash.digest():
+        raise TypeError("Signature didn't match")
+
+def generatePreMasterSecret():
     # The client generates a 48-byte pre-master secret
     # and encrypts with the public key from the server’s
     # certificate or temporary RSA key from a
@@ -54,32 +76,13 @@ def preMasterKey():
     return os.urandom(48)
     #TODO: generate pre-master key(48 byte encrypted with server's RSA public key)
 
-# 7. Receive Public Key from server
-def recvPublicKeyFromServer(s):
-    publickey = s.recv(1024)
-    print('Client received public key with certificate', publickey.decode("utf-8"))
-    return publickey
-
-# 9. Receive CipherText
-def recvCipherTextFromServer(s):
-    ciphertext = s.recv(1024)
-    print('Client received ciphertext', ciphertext)
+def encrypt(pk, plaintext):
+    ciphertext =  [RSA.encrypt(pk, b) for b in plaintext]
     return ciphertext
 
-def encrypt(s, publickey, plain_text):
-    cipher = PKCS1_OAEP.new(publickey)
-    cipher_text = cipher.encrypt(plain_text)
-    print('     Plain text to encrypt: ', plain_text )
-    print('     Cipher text to send to client: ', cipher_text)
-    return cipher_text
-
-# Assume client's private key is a saved file
-def decryption(cipher_text):
-    key = RSA.importKey(open('keyfile.pem').read())
-    cipher = PKCS1_OAEP.new(key)
-    plain_text = cipher.decrypt(cipher_text)
-    print('     Decrypted Message: ', repr(plain_text))
-    return plain_text
+def decrypt(pk, ciphertext):
+    plaintext = b''.join([RSA.decrypt(pk, c) for c in ciphertext])
+    return plaintext
 
 # 11. Receive Server_hello_done
 def recvServerHelloDone(s):
--- server.py
+++ server.py
@@ -4,13 +4,14 @@ import os
 import time
 import hashlib
 import RSA
+import random
+import des
 
-import Cryptodome
-from Cryptodome import Random
 from Cryptodome.Hash import SHA1
 
 HOST = '127.0.0.1'  # Localhost
 PORT = 3030         # Listening port
+thirdPartyPrivateKey, thirdPartyPublicKey  = RSA.generate_keypair(1297279, 1297657)
 
 def startServer():
     privatekey, publickey = rsakeys()
@@ -21,16 +22,16 @@ def startServer():
         conn, addr = s.accept()     # Blocking call
         allMsgsHash = SHA1.new()
         with conn:
-            flag, cipherSuit, tempMsg = recvClientHello(conn)   # 1. Handle Client_Hello
-            allMsgsHash.update(tempMsg)                         # Updates hash with each received message
+            flag, cipherSuit, tempMsg = recvClientHello(conn)       # 1. Recv Client_Hello
+            allMsgsHash.update(tempMsg)                             # Updates hash with each received message
             if flag is None:
                 print("Got error receiving Client hello")
                 return
-            allMsgsHash.update(sendServerHello(conn))           # 3. Send Server_Hello
-            allMsgsHash.update(sendCertificate(conn))           # 5. Send Certificate and Public key
-            sendPublicKey(conn, publickey)
-            allMsgsHash.update(encrypt(conn, publickey, b'here is software'))   # 8. Send encypted message to client
-            allMsgsHash.update(sendServerDone(conn))                            # 10. Send Server_hello_done
+            allMsgsHash.update(sendServerHello(conn))               # 2. Send Server_Hello
+            allMsgsHash.update(sendCertificate(conn, publickey))    # 3. Send Certificate and Public key
+            preMasterSecret = recvPreMasterSecret(conn, privatekey) # 3.5 Recv Premaster Key
+            allMsgsHash.update(sendServerDone(conn))                # 4. Send Server_hello_done
+            print(allMsgsHash.hexdigest())
 
 # 1. Handle Client_Hello
 def recvClientHello(conn):
@@ -48,56 +49,62 @@ def sendServerHello(s):
     return b'Server_Hello'
 
  # 5. Send Certificate and Public key
-def sendCertificate(s):
-    certificate = makeCertificate(b'Download Server')
-    print("Server received: Size of certificate = " ,len(certificate))
-    s.sendall(certificate)
-    # json.dumps(certificate)
-    return certificate
 
-def makeCertificate(name):
-    return b'''{ "Name": "%b",
-    "Serial Number":"12345678",
-    "Expiration Date":"12/12/2020"}''' % name
+def sendCertificate(s, pk):
+    certificate = makeServerCertificate('Download Server', pk)
+    
+    certSignature = makeCertificateSignature(certificate.encode("utf-8"))
+    completeCertificate = '''\
+        {"certificate": %s, \
+        "signature": "%s"}''' % (certificate, certSignature)
+    print("Server sent certificate, Size of certificate = ", len(completeCertificate))
+    s.sendall(completeCertificate.encode("utf-8"))
+    
+    return completeCertificate.encode("utf-8")
 
-def sendPublicKey(s, publickey):
-    #s.send(publickey.exportKey(format='PEM', passphrase=None, pkcs=1))
-    s.sendall(publickey.export_key('PEM'))
-    return publickey
+def makeServerCertificate(name, pk):
+    cert = '''{ "Name": "%s",\
+    "Serial Number":"12345678", \
+    "Expiration Date":"12/12/2020", \
+    "d": "%d", "n": "%d"}''' % (name, pk[0], pk[1])
+    return cert.replace(" ", "")
 
-#Private key and public key 1024 bits
+def makeCertificateSignature(certificate):
+    certificateHash = SHA1.new()
+    certificateHash.update(certificate)
+    print("Certificate: ", certificate)
+    print("Cert Hash: ", certificateHash.hexdigest())
+    encryptedHash = encrypt(thirdPartyPrivateKey,certificateHash.digest())
+    strListEncryptedHash = [str(i) for i in encryptedHash]
+    return ' '.join(strListEncryptedHash)
+
+def recvPreMasterSecret(s, pk):
+    encryptedPreMasterSecret = s.recv(1024) 
+    intArrOfPreMasterSecret = [int(n) for n in encryptedPreMasterSecret.decode("utf-8").split()]
+    preMasterSecret = decrypt(pk, intArrOfPreMasterSecret)
+    print("preMasterSecret : ", preMasterSecret)
+    return preMasterSecret
+
+#Private key and public key
 def rsakeys():
-     length=1024
-     privatekey = RSA.generate(length, Random.new().read)
-     publickey = privatekey.publickey()
+    primeNumbersFile = open("primeNumbers.txt", "r")
+    lines = primeNumbersFile.readlines()
+    randomLine = random.randint(0, len(lines)-1)
+    splitLine = lines[randomLine].split()
+    p = int(splitLine[0])  
+    q = int(splitLine[1])
+    return RSA.generate_keypair(p, q)
 
-     # Save private key as a file to be used for decryption
-     f = open('keyfile.pem', 'wb')
-     f.write(privatekey.exportKey('PEM'))
-     f.close()
+# Encryption with RSA Public Key, returns an array of integers
+def encrypt(pk, plaintext):
+    ciphertext =  [RSA.encrypt(pk, b) for b in plaintext]
+    return ciphertext
 
-     return privatekey, publickey
+def decrypt(pk, ciphertext):
+    plaintext = b''.join([RSA.decrypt(pk, c) for c in ciphertext])
+    return plaintext
 
-
-# 8. Send encypted message to client
-# Encryption with RSA Public Key
-def encrypt(s, publickey, plain_text):
-    cipher = PKCS1_OAEP.new(publickey)
-    cipher_text = cipher.encrypt(plain_text)
-    print('     Plain text to encrypt: ', plain_text )
-    print('     Cipher text to send to client: ', cipher_text)
-    s.sendall(cipher_text)
-    return cipher_text
-
-# Assume server's private key is a saved file
-def decryption(cipher_text):
-    key = RSA.importKey(open('keyfile.pem').read())
-    cipher = PKCS1_OAEP.new(key)
-    plain_text = cipher.decrypt(cipher_text)
-    print('     Decrypted Message: ', repr(plain_text))
-    return plain_text
-
- # 10. Send Server_hello_done
+# 10. Send Server_hello_done
 def sendServerDone(s):
     # Send serverHello message with right contents
     s.sendall(b'Server_Hello_Done')
