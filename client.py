b824413cff660250939f994807e82506b738238a
SCaicedo99 <43104210+SCaicedo99@users.noreply.github.com>
2020-11-18 18:20:21 -0500

    Client can request software and received a verified version of it from server

--- client.py
+++ client.py
@@ -4,10 +4,10 @@ import random
 import json
 import os
 import RSA
-
+import des
 
 from Cryptodome.Hash import SHA1
-
+from Cryptodome.Hash import MD5
 # Documentation for cryptography library https://cryptography.io/en/latest/
 HOST = '127.0.0.1'  # Localhost
 PORT = 3030         # Same port as server
@@ -17,52 +17,72 @@ def startClient():
     allMsgsHash = SHA1.new()
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((HOST, PORT))
-        allMsgsHash.update(sendClientHello(s))          # 1. Send Client_Hello
-        allMsgsHash.update(recvServerHello(s))          # 2. Receive Server_Hello
-        serverPublicKey, serverCertificate = recvCertiFromServer(s)
-        allMsgsHash.update(serverCertificate)      # 6. Receive Certificate from server and generate pre-master key
-        preMasterSecret = sendPreMasterSecret(s, serverPublicKey) 
-        allMsgsHash.update(recvServerHelloDone(s))      # 11. Receive Server_hello_done
+
+        cipherSuitBytes, cipherSuitjson = sendClientHello(s) # 1. Send Client_Hello
+        allMsgsHash.update(cipherSuitBytes)  
+
+        serverHelloBytes, serverHellojson = recvServerHello(s) # 2. Receive Server_Hello
+        allMsgsHash.update(serverHelloBytes)
+
+        # 3. Receive Certificate from server and generate pre-master key
+        serverPublicKey, serverCertificateBytes = recvCertiFromServer(s)
+        allMsgsHash.update(serverCertificateBytes)
+
+        preMasterSecretBytes = sendPreMasterSecret(s, serverPublicKey) # 3.5 Send Premaster key
+        allMsgsHash.update(preMasterSecretBytes)
+
+        allMsgsHash.update(recvServerHelloDone(s))      # 4. Receive Server_hello_done
+
+        masterSecret = createMasterSecret(preMasterSecretBytes,\
+             cipherSuitjson["randomNonce"].encode("utf-8"), serverHellojson["randomNonce"].encode("utf-8"))
+    
+        sendSoftwareRequest(s)
+
+        recvSoftware(s, masterSecret, serverPublicKey)
+        
         return
 
-# 2. Send Client_Hello
+
 def sendClientHello(s):
-    cypherSuit = b'{ "Cipher": "DES", "MAC":"SHA-1", "CipherType":"Block", "HashSize":"20"}'
-    s.sendall(cypherSuit)
-    return cypherSuit
+    n = 10
+    helloRandom = random.randint(pow(10, n), pow(10, n+1) -1) # Creates a random number of n digits
+    cipherSuitBytes = ('{ "Cipher": "DES", "MAC":"SHA-1",\
+        "CipherType":"Block", "HashSize":"20", "randomNonce":"%s"}' % str(helloRandom)).replace(" ","").encode("utf-8")
+    s.sendall(cipherSuitBytes)
+    cipherSuitjson = json.loads(cipherSuitBytes)
+    print("\n1. Client Hello: \n\t>>>", cipherSuitjson)
+    return cipherSuitBytes , cipherSuitjson
 
-# 4. Receive Server_Hello
 def recvServerHello(s):
-    serverHello = s.recv(1024) # Wait for message from Server
-    print('Client received (Server Hello), ', repr(serverHello))
-    return serverHello
+    serverHelloBytes = s.recv(1024) # Wait for message from Server
+    serverHellojson = json.loads(serverHelloBytes)
+    print('\n2. Server Hello: \n\t>>>', serverHellojson)
+    return serverHelloBytes, serverHellojson
 
-# 6. Receive Certificate from server
 def recvCertiFromServer(s):
-    rawCertificate = s.recv(500)
-    jsonCert = json.loads(rawCertificate)
-    print('Client received certificate', jsonCert)
+    serverCertificateBytes = s.recv(500)
+    jsonCert = json.loads(serverCertificateBytes)
+    print('\n3. Receive server certificate:\n\t>>>', jsonCert)
     certificate = jsonCert["certificate"]
     signature = jsonCert["signature"]
     serverPublicKey = (int(certificate["d"]), int(certificate["n"]))
-    verifySignature(certificate, signature)
+    verifySignature(json.dumps(certificate).replace(" ", ""), signature, thirdPartyPublicKey)
 
-    return serverPublicKey, rawCertificate
-    #TODO: generate pre-master key(48 byte encrypted with server's RSA public key)
+    return serverPublicKey, serverCertificateBytes
 
 def sendPreMasterSecret(s, pk):
     preMasterSecret = generatePreMasterSecret()
-    print("preMasterSecret : ", preMasterSecret)
+    print("\n3.5 Send premasterSecret (encrypted):\n\t>>>", preMasterSecret)
     temp = encrypt(pk, preMasterSecret)
     encryptedPreMasterSecret = ' '.join([str(n) for n in temp])
     s.sendall(encryptedPreMasterSecret.encode("utf-8"))
     return preMasterSecret
 
-def verifySignature(contents, signature):
+def verifySignature(contents, signature, pk):
     certHash = SHA1.new()
-    certHash.update(json.dumps(contents).replace(" ","").encode("utf-8"))
+    certHash.update(contents.encode("utf-8"))
     intArrOfSignature = [int(n) for n in signature.split()]
-    decryptedHash = decrypt(thirdPartyPublicKey, intArrOfSignature)
+    decryptedHash = decrypt(pk, intArrOfSignature)
     if decryptedHash != certHash.digest():
         raise TypeError("Signature didn't match")
 
@@ -84,10 +104,63 @@ def decrypt(pk, ciphertext):
     plaintext = b''.join([RSA.decrypt(pk, c) for c in ciphertext])
     return plaintext
 
-# 11. Receive Server_hello_done
 def recvServerHelloDone(s):
-    serverDone = s.recv(96)
-    print("Client received (Server_Hello_Done) ", repr(serverDone))
-    return serverDone
+    serverHelloDoneBytes = s.recv(96)
+    print("\n4. Receive server Hello done: \n\t>>>",serverHelloDoneBytes)
+    return serverHelloDoneBytes
+
+def createMasterSecret(preMasterSecretBytes, clientRandom, serverRandom):
+    partOne = md5Helper([preMasterSecretBytes, shaHelper([b'A', preMasterSecretBytes,\
+        clientRandom, serverRandom])])
+    partTwo = md5Helper([preMasterSecretBytes, shaHelper([b'BB', preMasterSecretBytes,\
+        clientRandom, serverRandom])])
+    partThree = md5Helper([preMasterSecretBytes, shaHelper([b'CCC', preMasterSecretBytes,\
+        clientRandom, serverRandom])])
+    masterSecret = b''.join([partOne, partTwo, partThree])
+
+    print("\n MasterSecret = \n\t>>>", masterSecret)
+    return masterSecret
+
+def shaHelper(byteArr):
+    mySHA = SHA1.new()
+    mySHA.update(b''.join(byteArr))
+    return mySHA.digest()
+
+def md5Helper(byteArr):
+    myMD5 = MD5.new()
+    myMD5.update(b''.join(byteArr))
+    return myMD5.digest()
+
+def sendSoftwareRequest(s):
+    request = b'{"File Name" : "penguin.png"}'
+    s.sendall(request)
+    print("\n5. Client send software request:\n\t", request)
+
+def recvSoftware(s, desKey, serverPublicKey):
+    infoAboutPackets = json.loads(s.recv(100))
+    with open("newPenguin.png", 'wb') as newFile:
+        for packetN in range(int(infoAboutPackets["Number of packets"])):
+            if packetN % 50 == 0:
+                print(f"Receiving packet #{packetN}...")
+            rcvdPacket = json.loads(s.recv(1024))
+            actualPacketN = rcvdPacket["Payload"]["Packet Number"]
+            verifySignature(json.dumps(rcvdPacket["Payload"]).replace(" ", ""),\
+                rcvdPacket["Signature"], serverPublicKey)
+            newFile.write(decryptDEShelper(rcvdPacket["Payload"]["Data"], desKey))
+            if packetN % 50 == 0:
+                print(f"Received packet #{actualPacketN}")
+            sendPacketConfirmation(s, packetN)
+
+def sendPacketConfirmation(s, n):
+    s.sendall(b'{"Packet Number": "%d"}' % n)
+
+def decryptDEShelper(ciphertext, key):
+    coder = des.des()
+    cipherRbyte = bytearray()
+    [cipherRbyte.append(int(ch)) for ch in ciphertext.split()]
+    rawPlaintext = coder.decrypt(key, cipherRbyte, padding=True)
+    rByte = bytearray()
+    [rByte.append(ord(ch)) for ch in rawPlaintext]
+    return rByte
 
 startClient()
--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import random
 import des
 
 from Cryptodome.Hash import SHA1
+from Cryptodome.Hash import MD5
 
 HOST = '127.0.0.1'  # Localhost
 PORT = 3030         # Listening port
@@ -22,31 +23,43 @@ def startServer():
         conn, addr = s.accept()     # Blocking call
         allMsgsHash = SHA1.new()
         with conn:
-            flag, cipherSuit, tempMsg = recvClientHello(conn)       # 1. Recv Client_Hello
-            allMsgsHash.update(tempMsg)                             # Updates hash with each received message
-            if flag is None:
-                print("Got error receiving Client hello")
-                return
-            allMsgsHash.update(sendServerHello(conn))               # 2. Send Server_Hello
+            cipherSuitBytes, cipherSuitjson = recvClientHello(conn)  # 1. Recv Client_Hello
+            allMsgsHash.update(cipherSuitBytes)                      # Updates hash with each received message
+
+            clientHelloBytes, hellojson = sendServerHello(conn)      # 2. Send Server_Hello
+            allMsgsHash.update(clientHelloBytes)
+                           
             allMsgsHash.update(sendCertificate(conn, publickey))    # 3. Send Certificate and Public key
-            preMasterSecret = recvPreMasterSecret(conn, privatekey) # 3.5 Recv Premaster Key
+            
+
             allMsgsHash.update(sendServerDone(conn))                # 4. Send Server_hello_done
+
+            preMasterSecretBytes = recvPreMasterSecret(conn, privatekey) # 5. Recv Premaster Key
+            allMsgsHash.update(preMasterSecretBytes)
+
             print(allMsgsHash.hexdigest())
 
-# 1. Handle Client_Hello
-def recvClientHello(conn):
-    data = conn.recv(1024)
-    if data is None:
-        print("Did not get any data")
-        return None, None, None
-    cipherSuit = json.loads(data) # Contains a dictionary with the cyphersuit
-    return 1, cipherSuit, data
+            masterSecret = createMasterSecret(preMasterSecretBytes, \
+                cipherSuitjson["randomNonce"].encode("utf-8"), hellojson["randomNonce"].encode("utf-8"))
+            
+            fileName = recvSoftwareRequest(conn)                 # 6. Recv software request from clients
 
-# 3. Send Server_Hello
+            sendSoftware(conn, fileName, masterSecret, privatekey)
+
+def recvClientHello(conn):
+    cipherSuitBytes = conn.recv(1024)
+    cipherSuitjson = json.loads(cipherSuitBytes) # Contains a dictionary with the cyphersuit
+    print("\n1. Client Hello: \n\t>>>", cipherSuitjson)
+    return cipherSuitBytes, cipherSuitjson
+
 def sendServerHello(s):
-    # Send serverHello message with right contents
-    s.sendall(b'Server_Hello')
-    return b'Server_Hello'
+    n = 10
+    helloRandom = random.randint(pow(10, n), pow(10, n+1) -1) # Creates a random number of n digits
+    helloBytes = ('{"Message": "Server Hello", "randomNonce": "%s"}' % str(helloRandom)).replace(" ","").encode("utf-8") 
+    s.sendall(helloBytes)
+    hellojson = json.loads(helloBytes)
+    print("\n2. Server Hello: \n\t>>>", hellojson)
+    return helloBytes, hellojson
 
  # 5. Send Certificate and Public key
 
@@ -57,7 +70,7 @@ def sendCertificate(s, pk):
     completeCertificate = '''\
         {"certificate": %s, \
         "signature": "%s"}''' % (certificate, certSignature)
-    print("Server sent certificate, Size of certificate = ", len(completeCertificate))
+    print("\n3. Send server Certificate: \n\t>>>", json.loads(completeCertificate))
     s.sendall(completeCertificate.encode("utf-8"))
     
     return completeCertificate.encode("utf-8")
@@ -72,8 +85,6 @@ def makeServerCertificate(name, pk):
 def makeCertificateSignature(certificate):
     certificateHash = SHA1.new()
     certificateHash.update(certificate)
-    print("Certificate: ", certificate)
-    print("Cert Hash: ", certificateHash.hexdigest())
     encryptedHash = encrypt(thirdPartyPrivateKey,certificateHash.digest())
     strListEncryptedHash = [str(i) for i in encryptedHash]
     return ' '.join(strListEncryptedHash)
@@ -82,7 +93,7 @@ def recvPreMasterSecret(s, pk):
     encryptedPreMasterSecret = s.recv(1024) 
     intArrOfPreMasterSecret = [int(n) for n in encryptedPreMasterSecret.decode("utf-8").split()]
     preMasterSecret = decrypt(pk, intArrOfPreMasterSecret)
-    print("preMasterSecret : ", preMasterSecret)
+    print("\n3.5 Receive premasterSecret (encrypted):\n\t>>>", preMasterSecret)
     return preMasterSecret
 
 #Private key and public key
@@ -104,11 +115,93 @@ def decrypt(pk, ciphertext):
     plaintext = b''.join([RSA.decrypt(pk, c) for c in ciphertext])
     return plaintext
 
-# 10. Send Server_hello_done
 def sendServerDone(s):
     # Send serverHello message with right contents
     s.sendall(b'Server_Hello_Done')
     return b'Server_Hello_Done'
+
+def createMasterSecret(preMasterSecretBytes, clientRandom, serverRandom):
+    partOne = md5Helper([preMasterSecretBytes, shaHelper([b'A', preMasterSecretBytes,\
+        clientRandom, serverRandom])])
+    partTwo = md5Helper([preMasterSecretBytes, shaHelper([b'BB', preMasterSecretBytes,\
+        clientRandom, serverRandom])])
+    partThree = md5Helper([preMasterSecretBytes, shaHelper([b'CCC', preMasterSecretBytes,\
+        clientRandom, serverRandom])])
+    masterSecret = b''.join([partOne, partTwo, partThree])
+
+    print("\n MasterSecret = \n\t>>>", masterSecret)
+    return masterSecret
+
+def shaHelper(byteArr):
+    mySHA = SHA1.new()
+    mySHA.update(b''.join(byteArr))
+    return mySHA.digest()
+
+def md5Helper(byteArr):
+    myMD5 = MD5.new()
+    myMD5.update(b''.join(byteArr))
+    return myMD5.digest()
+
+def recvSoftwareRequest(s):
+    softwareRequest = s.recv(30)
+    print("\n5. Server receives software request:\n\t>>>", softwareRequest)
+    return json.loads(softwareRequest)["File Name"]
+
+def sendSoftware(s, fileName, desKey, privateRSAKey):
+    with open(fileName, "rb") as rawFile:
+        softwareFile = rawFile.read()
+        #Splits the software file into a sublist of length n.
+        n = 124 
+        splitFile = [softwareFile[k:k+n] for k in range(0, len(softwareFile), n)]
+        sendInfoAboutPackets(s, fileName, len(splitFile))
+        for packetN in range(len(splitFile)):
+            if packetN % 50 == 0:
+                print(f"Sending packet #{packetN}...")
+            packetToSend = createPacket(splitFile[packetN], desKey, privateRSAKey, packetN)
+            s.sendall(packetToSend)
+            if packetN % 50 == 0:
+                print(f"Waiting for ACK on packet {packetN}...")
+            if not getPacketConfirmation(s, packetN):
+                print("Got wrong confirmation packet, cancelling...")
+                return
+    
+def sendInfoAboutPackets(s, fileName, m):
+    info = b'{"File Name": "%b", "Number of packets": "%d"}' % (fileName.encode("utf-8"), m)
+    s.sendall(info)
+
+def createPacket(data, desKey, privateRSAKey,n):
+    encryptedData = encryptDEShelper(data, desKey)
+    rawPacket = '{"Data":"%s","Packet Number":"%s"}' % (encryptedData, str(n))
+    signature = getPacketSignature(rawPacket.replace(" ","").encode("utf-8"), privateRSAKey)
+    finalPacket = ('{"Payload" : %s, "Signature": "%s"}' % (rawPacket, signature)).encode("utf-8")
+    return finalPacket
+
+def getPacketSignature(content, pk):
+    certificateHash = SHA1.new()
+    certificateHash.update(content)
+    encryptedHash = encrypt(pk,certificateHash.digest())
+    strListEncryptedHash = [str(i) for i in encryptedHash]
+    return ' '.join(strListEncryptedHash)
+
+# This returns a string of numbers that need to be converted to bytearray to decrypt
+def encryptDEShelper(data, key):
+    coder = des.des()
+    rawCipher = coder.encrypt(key, data, padding=True)
+    rByte = bytearray()
+    [rByte.append(ord(ch)) for ch in rawCipher]
+    temp = [str(i) for i in rByte]
+    return ' '.join(temp)
+
+def decryptDEShelper(ciphertext, key):
+    pass
+
+def getPacketConfirmation(s, n):
+    ACKn = json.loads(s.recv(100))
+    if ACKn["Packet Number"] != str(n):
+        print(f"Did not get ACK for packet #{n}\nGot:")
+        print("\t",ACKn)
+        return False
+    return True
 
 print('Server listening on: ', PORT)
 startServer()
